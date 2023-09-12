c7fd10f95b2744e1fc714153cc53328eab244b24
Chicho221 <135489598+Chicho221@users.noreply.github.com>
2023-09-12 02:55:00 +0200

    Add Listener for changing background-color of squeres.

--- scripts.js
+++ scripts.js
@@ -1,14 +1,23 @@
 const DEFAULT_SIZE = 16; //Default size of a grid
 
 let gridSize = DEFAULT_SIZE;
-
-const container = document.querySelector('#container');
-
 let squereWidth = 600/gridSize + 'px';
 let squereHeight = 600/gridSize + 'px';
-for(let i = 1; i < gridSize ** 2; i++){ //Creates squeres to fill board
-    const gridSquere = document.createElement('div');
-    gridSquere.classList.add('gridSquere');
-    gridSquere.style.cssText = `width:${squereWidth}; height:${squereHeight};`;
-    container.appendChild(gridSquere);
+const container = document.querySelector('#container');
+
+function createGrid(){
+    for(let i = 0; i < gridSize ** 2; i++){ //Creates squeres to fill board
+        const gridSquere = document.createElement('div');
+        gridSquere.classList.add('gridSquere');
+        gridSquere.style.cssText = `width:${squereWidth}; height:${squereHeight};`;
+        
+        gridSquere.addEventListener('mouseover',() =>{ //On hover changes background color
+            gridSquere.style.cssText += 'background-color: blue';
+        });
+        
+        container.appendChild(gridSquere);
+
+    }  
 }
+
+window.onload = createGrid();
