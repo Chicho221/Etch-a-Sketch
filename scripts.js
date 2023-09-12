cf3aacae8460b9d6a0de8176c7d9dca154186a9d
Chicho221 <135489598+Chicho221@users.noreply.github.com>
2023-09-12 03:53:59 +0200

    Add button to change size of a grid
    Add function to change size of a grid with user input.

--- scripts.js
+++ scripts.js
@@ -1,12 +1,21 @@
 const DEFAULT_SIZE = 16; //Default size of a grid
 
 let gridSize = DEFAULT_SIZE;
-let squereWidth = 600/gridSize + 'px';
-let squereHeight = 600/gridSize + 'px';
 const container = document.querySelector('#container');
+let changeValue = document.querySelector('#changeSize');
+
+changeValue.addEventListener('click',() =>{
+    
+    changeValue = prompt('Enter amount of squeres per side (max:100):');
+    gridSize = parseInt(changeValue);
+    reset();
+    createGrid(gridSize);
+});
 
 function createGrid(){
     for(let i = 0; i < gridSize ** 2; i++){ //Creates squeres to fill board
+        let squereWidth = 600/gridSize + 'px';
+        let squereHeight = 600/gridSize + 'px';
         const gridSquere = document.createElement('div');
         gridSquere.classList.add('gridSquere');
         gridSquere.style.cssText = `width:${squereWidth}; height:${squereHeight};`;
@@ -16,8 +25,13 @@ function createGrid(){
         });
         
         container.appendChild(gridSquere);
-
     }  
 }
 
 window.onload = createGrid();
+
+function reset() {
+    while (container.firstChild) {
+      container.removeChild(container.lastChild);
+    }
+}
